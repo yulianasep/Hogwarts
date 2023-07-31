4b40dfc164a2fa705b4fc58161cd6ed1be10300e
yulianasep <yuliana.sepulveda@alternova.com>
2023-07-31 10:40:49 -0500

    refactor: updating urls

--- apps/houses/api/serializers.py
+++ apps/houses/api/serializers.py
@@ -8,9 +8,20 @@ class HouseSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        name_body = validated_data.get("name")
+        name = validated_data.get("name")
+        mascot = validated_data.get("mascot")
+        head_of_house = validated_data.get("head_of_house")
+        house_ghost = validated_data.get("house_ghost")
+        founder = validated_data.get("founder")
+        school = validated_data.get("school")
+
         new_house= House.objects.create(
-            name=name_body
+            name=name,
+            mascot=mascot,
+            head_of_house=head_of_house,
+            house_ghost=house_ghost,
+            founder=founder,
+            school=school
         )
         return new_house
     
